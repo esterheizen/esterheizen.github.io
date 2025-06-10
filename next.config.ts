33a72953d4cfa6c34dabc0cfc6561c86dc039c4f
esterheizen <shafkattashrif@gmail.com>
2025-06-10 17:50:23 +1000

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactStrictMode: true,
-  output: "export", // Enable static HTML export
-  images: { unoptimized: true }, // Needed if you use <Image />
 };
 
 export default nextConfig;
